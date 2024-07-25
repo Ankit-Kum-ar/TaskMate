6ae331f91a86bc30abca1ad1ff7db9093830ff9f
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-26 01:25:28 +0530

    Changing the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,8 @@ const Header = () => {
             <h1 className="text-[22px] font-sans mt-4 text-gray-600">Taskmate</h1>
         </div>
 
-        <div className="flex mt-[2%] gap-1 px-3 ">
-            <button className="rounded-full bg-white w-[22px] h-[22px] border-2 hover:border-[3.3px] " ></button>
-            <button className="rounded-full bg-[#EFEFEF] w-[22px] h-[22px] border-2"></button>
-            <button className="rounded-full bg-black w-[22px] h-[22px] border-2"></button>
-            <button className="rounded-full bg-custom-gradient-1 w-[22px] h-[22px] border-2"></button>
-            <button className="rounded-full bg-red-900 w-[22px] h-[22px] border-2"></button>
-            <button className="rounded-full bg-lime-950 w-[22px] h-[22px] border-2 "></button>
+        <div className="flex p-3 ">
+            <button className="bg-blue-700 text-white p-2 rounded-md font-medium px-3">Welcome Back</button>
         </div>
     </div>
   )
